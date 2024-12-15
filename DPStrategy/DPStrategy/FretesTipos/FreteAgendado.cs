7e44777281258fea111550f4912ffc4f558444c9
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-12-14 21:05:05 -0300

    change folder structure

---
+++ DPStrategy/DPStrategy/FretesTipos/FreteAgendado.cs
@@ -0,0 +1,9 @@
+﻿namespace DPStrategy.FretesTipos;
+
+public class FreteAgendado : IFrete
+{
+    public double Calcula(double valor)
+    {
+        return valor * 0.08;
+    }
+}
--- DPStrategy/DPStrategy/Program.cs
+++ DPStrategy/DPStrategy/Program.cs
@@ -3,6 +3,7 @@ using DPStrategy.FretesTipos;
 
 FreteComum freteComum = new FreteComum();
 FreteExpresso freteExpresso = new FreteExpresso();
+FreteAgendado freteAgendado = new FreteAgendado();
 
 Pedido pedido1 = new Pedido(100, freteComum);
 Pedido pedido2 = new Pedido(200, freteComum);
@@ -18,3 +19,5 @@ pedido2.PrintInformation();
 pedido2.SetFrete(freteExpresso);
 
 pedido2.PrintInformation();
+
+Pedido pedido3 = new Pedido(300, freteAgendado);
